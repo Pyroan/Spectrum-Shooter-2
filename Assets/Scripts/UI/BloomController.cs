ffcd1a3cfe57facc4535aad3543ace684176c8cf
Violet Moore <Pyroan@users.noreply.github.com>
2020-05-20 21:03:13 -0700

    Make menus BEAUTIFUL

---
+++ Assets/Scripts/UI/BloomController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+/**
+ *  Handles bloom in stuff like menus.
+ */
+public class BloomController : MonoBehaviour
+{
+    Bloom bloom;
+    public float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PostProcessVolume volume = GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings(out bloom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bloom.color.value = Color.HSVToRGB(((Time.time * 30 + offset) % 360) / 360, 1, 1);
+    }
+}
