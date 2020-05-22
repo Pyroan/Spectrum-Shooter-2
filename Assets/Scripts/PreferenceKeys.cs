63ef1dacf2ef6880d211533b0cef1b344444109f
Evan Schoenberger <evanjschoenberger@gmail.com>
2020-05-21 20:56:02 -0700

    make options menu exist. i hated it

---
+++ Assets/Scripts/PreferenceKeys.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Constants for keys in playerprefs. Safety baby.
+ * oh wait this is COMPLETELY useless.
+ */
+public class PreferenceKeys : MonoBehaviour
+{
+
+    public const string FLASH_COLORS = "flash_colors";
+    public const string SHAKE_SCREEN = "shake_screen";
+    public const string CONTROL_HINTS = "control_hints";
+
+    public const string PLAY_MUSIC = "music_enabled";
+    public const string PLAY_SOUND = "sound_enabled";
+}
---
+++ Assets/Scripts/UI/OptionsHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OptionsHandler : MonoBehaviour
+{
+
+    public void Confirm()
+    {
+        // PlayerPrefs.Save();
+        SceneManager.LoadScene(0);
+    }
+
+    public void Cancel()
+    {
+        // fuck how do i invert this.
+        SceneManager.LoadScene(0);
+    }
+}
