e9db87ded2408fed9f8c04f1c096648e4e37e018
Evan Schoenberger <evanjschoenberger@gmail.com>
2020-05-23 11:17:37 -0700

    add epilepsy warning

---
+++ Assets/Scripts/EpilepsyScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EpilepsyScreen : MonoBehaviour
+{
+    public float timeOnscreen = 10f;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = FindObjectOfType<Animator>();
+        StartCoroutine(Display());
+    }
+
+    IEnumerator Display()
+    {
+        yield return new WaitForSeconds(timeOnscreen);
+        animator.SetTrigger("Fade Out");
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(0);
+
+    }
+}
